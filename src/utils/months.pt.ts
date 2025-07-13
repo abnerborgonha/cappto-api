40cea8bad681d7dd0209dc5c4e254106be978b3e
Abnêr Borgonha <abnerborgonha@gmail.com>
2025-07-13 15:51:20 -0300

    feat: add months pt util static

---
+++ src/utils/months.pt.ts
@@ -0,0 +1,14 @@
+export const months = Object.freeze([
+  'janeiro',
+  'fevereiro',
+  'março',
+  'abril',
+  'maio',
+  'junho',
+  'julho',
+  'agosto',
+  'setembro',
+  'outubro',
+  'novembro',
+  'dezembro',
+])
